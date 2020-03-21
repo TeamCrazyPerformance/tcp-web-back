71ae8e25a19c2c3b6c1754d2241faaa91e96ea9c
taening <m9472757c@gmail.com>
2020-03-21 11:39:23 +0900

    feat: #2 회원가입 API를 구현했습니다
    user/urls.py 에 api/users/signup/ URL경로를 추가했습니다.
    해당 경로로 POST를 보내면 해당 정보를 DB에 등록하는 기능을 구현했습니다.
    user/views.py 에서 구현한 코드를 확인할 수 있습니다.
    
    tcpsite/urls.py 에 불필요한 Module 임포트를 제거했습니다.
    user/models.py 에 Autopep8이 실행되어 NewLine이 추가됐습니다.

--- tcpsite/tcpsite/urls.py
+++ tcpsite/tcpsite/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
-from rest_framework import routers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- tcpsite/user/serializer.py
+++ tcpsite/user/serializer.py
@@ -1,15 +1,8 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from .models import *
 
 
-class GradeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Grade
-        fields = '__all__'
-
-
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-
